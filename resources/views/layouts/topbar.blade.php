33ed358624c019856e75549c326ac455dfb05f72
Reynaldi Rahadian <astrxl0rd@users.noreply.github.com>
2020-04-20 10:25:38 +0700

    Update topbar.blade.php

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -1,4 +1,4 @@
-s<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" ><img src="img/logo.png" width="50"/></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
